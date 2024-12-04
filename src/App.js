12fe3db509c2f53ba693e42996d63750ecf79369
SrijitBanerjee <srijitbanerjee867@gmail.com>
2024-12-04 16:06:26 +0530

    frontend is done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header/header.jsx';
+import Footer from './Components/Footer/footer.jsx';
+function App() {
+  return (
+    <div>
+       <Header/>
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
